76fd003f8e7cc88a7a38fdec55e0ec4251a3fafe
Taylor Beseda <tbeseda@gmail.com>
2022-03-31 14:19:33 -0600

    working on layout

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -13,11 +13,27 @@ async function handler() {
     views: path.join(__dirname, 'node_modules', '@architect/views'),
   });
 
+  const profile = github?.data?.user || {
+    user: {
+      avatarUrl:
+        'https://avatars.githubusercontent.com/u/15697?u=64bcbfb2f2ce642a7a7294fcaffd01ef986508e0&v=4',
+      bio: 'Colorado Front Range. Node.js thrill-seeker.',
+      company: '@beginner-corp ',
+      location: 'Longmont, CO',
+      login: 'tbeseda',
+      name: 'Taylor Beseda',
+      url: 'https://github.com/tbeseda',
+    },
+  };
+
+  // console.log(profile);
+
   return {
     html: await eta.renderFile('index.html', {
       fortnite,
       github,
       letterboxd,
+      profile,
     }),
   };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/views/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'begin-coral': '#fd6d6dff',
+        'begin-orange': '#fe8e20ff',
+        'colorado-blue': '#002147ff',
+        'green-pickup': '#007932ff',
+      },
+    },
   },
   plugins: [],
 };
