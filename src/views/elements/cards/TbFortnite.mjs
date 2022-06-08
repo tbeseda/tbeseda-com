a20ac33ec000152f06c0e5abdc02e95aa3ee66c8
Taylor Beseda <tbeseda@gmail.com>
2022-06-07 18:45:17 -0600

    update Enhance

--- src/views/elements/cards/TbFortnite.mjs
+++ src/views/elements/cards/TbFortnite.mjs
@@ -1,5 +1,6 @@
 export default function TbFortnite({ html, state }) {
   const { fortnite } = state?.store || {};
+  const currentStats = fortnite.stats?.p9;
 
   return html`
     <flip-card>
@@ -9,14 +10,14 @@ export default function TbFortnite({ html, state }) {
           Current season
         </h2>
         <h3 class="text-begin-coral font-medium sm:text-4xl text-3xl mb-1">
-          ${fortnite.stats.curr_p9.top1.value}
+          ${currentStats?.top1?.value || '?'}
         </h3>
         <p class="leading-relaxed text-gray-300">#1 Wins</p>
       </div>
 
       <div slot="back">
         <h3 class="text-green-pickup font-medium sm:text-4xl text-3xl mb-1">
-          ${fortnite.stats.curr_p9.kd.value}
+          ${currentStats?.kd?.value || '?'}
         </h3>
         <p class="leading-relaxed text-gray-600">K/D</p>
       </div>
