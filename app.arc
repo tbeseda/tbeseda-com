666068aee4239429618a785f455e1626c5fbc31b
Taylor Beseda <tbeseda@gmail.com>
2022-12-22 16:55:25 -0700

    use Enhance

--- app.arc
+++ app.arc
@@ -1,19 +1,18 @@
 @app
 tbeseda-com
 
-@aws
-region us-east-1
-runtime nodejs16.x
-architecture arm64
+@plugins
+enhance/arc-plugin-enhance
 
 @static
 fingerprint true
 prune true
 
+@enhance-styles
+config ./enhance-styles.json
+
 @http
-get /
 post /
-get /echo
 
 @scheduled
 update-things rate(1 hour)
@@ -22,14 +21,7 @@ update-things rate(1 hour)
 things
   thingID *String
 
-@plugins
-arc-plugin-tailwindcss
-# architect/plugin-bundles
-architect/plugin-lambda-invoker
-
-# @bundles
-# "base-element" "/node_modules/@enhance/base-element/index.mjs"
-
-@tailwindcss
-src src/styles/index.css
-minify true
+@aws
+region us-east-1
+runtime nodejs18.x
+architecture arm64
---
+++ app/head.mjs
@@ -0,0 +1,27 @@
+import { getStyles } from '@enhance/arc-plugin-styles'
+
+/** @type {import('@enhance/types').EnhanceHeadFn} */
+export default function Head(state) {
+  const { req } = state
+  const { path } = req
+  const title = `tbeseda.com ${path}`
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>${title}</title>
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏔️</text></svg>"/>
+      ${getStyles.linkTag()}
+      <style>
+        body > * {
+          max-width: 55rem;
+          margin: 0 auto;
+        }
+      </style>
+    </head>
+    <body class="font-sans">
+  `
+}
