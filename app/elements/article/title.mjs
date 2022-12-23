3b1c955b84340100909b745c0e59820b1079e1cc
Taylor Beseda <tbeseda@gmail.com>
2022-12-23 11:38:35 -0700

    add articles index API handler

--- app/elements/article/title.mjs
+++ app/elements/article/title.mjs
@@ -4,7 +4,7 @@ export default function ({ html, state: { store } }) {
 
 	return html`
     <h1 class="flex flex-row justify-start items-center gap-4">
-      <span class="p-name text3 font-semibold">${article.title}</span>
+      <span class="p-name text3 font-semibold"><slot></slot> ${article.title}</span>
       <a class="text-1 opacity-50 no-underline" href="/articles/${article.slug}">🔗</a>
     </h1>
   `
