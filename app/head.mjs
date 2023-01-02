3331ea1280d33fc204908c79c8596c7b47b9e545
Taylor Beseda <tbeseda@gmail.com>
2023-01-02 11:47:27 -0700

    unblock hljs css

--- app/head.mjs
+++ app/head.mjs
@@ -6,6 +6,8 @@ export default function Head(state) {
 	const { path } = req
 	const { icon } = store
 	const title = `Taylor Beseda ${path}`
+	const hljsThemeCss =
+		'https://unpkg.com/@highlightjs/cdn-assets@11.7.0/styles/night-owl.min.css'
 
 	return /* html */ `
 		<!DOCTYPE html>
@@ -18,7 +20,11 @@ export default function Head(state) {
 
 			<link rel="icon" href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${icon}</text></svg>'/>
 			<link rel="alternate" type="application/rss+xml" title="tbeseda.com Articles" href="/articles/rss"/>
-			<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/styles/night-owl.min.css"/>
+
+			<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="${hljsThemeCss}"/>
+			<noscript>
+				<link rel="stylesheet" href="${hljsThemeCss}"/>
+			</noscript>
 
 			${process.env.ARC_SANDBOX ? getStyles.linkTag() : getStyles.styleTag()}
 
