56787a9a04c4cea55bcd8412f1b8d008852f0b10
Taylor Beseda <tbeseda@gmail.com>
2023-06-17 19:39:13 -0600

    update vrite published webhook

--- src/http/post-webhooks-vrite/index.mjs
+++ src/http/post-webhooks-vrite/index.mjs
@@ -13,16 +13,36 @@ export async function handler({ body }) {
 	if (!VRITE_KEY) throw new Error('Missing Vrite key')
 	const vrite = createClient({ token: VRITE_KEY })
 
-	const contentPiece = await vrite.contentPieces.get({
-		id: '[CONTENT_PIECE_ID]',
-		content: true,
-	})
+	let contentPiece
+	try {
+		contentPiece = await vrite.contentPieces.get({ id, content: true })
+	} catch (error) {
+		console.log('Error fetching content piece', JSON.stringify(error, null, 2))
+	}
 
-	await things.put({
-		key: `vrite:content:${contentPiece.id}`,
-		updatedAt: new Date().toISOString(),
-		...contentPiece,
-	})
+	if (contentPiece) {
+		const newContent = Object.keys(contentPiece).reduce((acc, key) => {
+			const value = contentPiece[key]
+			if (value) acc[key] = value
+			return acc
+		}, {})
+
+		try {
+			await things.put({
+				key: `vrite:content:${contentPiece.id}`,
+				updatedAt: new Date().toISOString(),
+				...newContent,
+			})
+		} catch (err) {
+			console.log(
+				'Error saving content to db',
+				JSON.stringify(newContent, null, 2),
+				JSON.stringify(err, null, 2),
+			)
+		}
+	} else {
+		console.log('No content piece found', JSON.stringify(contentPiece, null, 2))
+	}
 
 	return {
 		statusCode: 200,
