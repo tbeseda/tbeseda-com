0a2258b9c7b3b75ace4d695d7c2e7afe76c48dad
Taylor Beseda <tbeseda@gmail.com>
2023-07-04 21:20:52 -0600

    Update pm2html-renderer.mjs

--- app/lib/pm2html-renderer.mjs
+++ app/lib/pm2html-renderer.mjs
@@ -1,8 +1,32 @@
 import { Renderer, AddBreaksToEmptyTextblocks } from 'pm2html'
 import { getSchema } from '@tiptap/core'
+import Highlight from '@tiptap/extension-highlight'
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 import StarterKit from '@tiptap/starter-kit'
+import Superscript from '@tiptap/extension-superscript'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 
-const schema = getSchema([StarterKit])
+const schema = getSchema([
+	StarterKit,
+	Highlight,
+	Image.configure({
+		allowBase64: true,
+	}),
+	Link.configure({
+		HTMLAttributes: {
+			target: '_blank',
+		},
+	}),
+	Superscript,
+	Table,
+	TableCell,
+	TableHeader,
+	TableRow,
+])
 
 export const renderer = new Renderer({
 	schema,
