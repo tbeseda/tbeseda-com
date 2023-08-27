b245c46fca2937fc90a3bf63d9b27a7156ca20bd
Taylor Beseda <tbeseda@gmail.com>
2023-08-27 16:04:29 -0600

    omnivore ingest for label, page, and HIGHLIGHT

--- src/http/post-webhooks-omnivore/index.mjs
+++ src/http/post-webhooks-omnivore/index.mjs
@@ -1,20 +1,32 @@
 import arc from '@architect/functions'
 
 const { things } = await arc.tables()
-const typeString = 'omnivore:unknown'
+const typePrefix = 'omnivore'
 
 async function http({ body }) {
-	console.log(body)
+	// search body for one of three keys: "label", "page", or "HIGHLIGHT"
+	const entity = body.label || body.page || body.HIGHTLIGHT || body.highlight
+	//                        in case the schema changes to lowercase ↑
+
+	if (!entity || !entity.type) return { status: 200 }
+
+	const type = entity.type.toLowerCase()
+	const fullType = `${typePrefix}:${type}`
+
+	entity.relatedId = entity.pageId || entity.articleId || entity.id
+	//                        label            HIGHLIGHT           page
+
+	delete entity.originalHtml // could be huge
 
 	await things.put({
-		key: `${typeString}:${new Date().toISOString()}`,
-		type: typeString,
+		key: `${fullType}:${entity.id}`,
+		type: fullType,
 		updatedAt: new Date().toISOString(),
-		body,
+		entity,
 	})
 
 	return {
-		statusCode: 200,
+		status: 200,
 		text: 'tyvm',
 	}
 }
