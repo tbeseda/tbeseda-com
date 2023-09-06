95b2f4925c011d36320f4060507c7b1123b91113
Taylor Beseda <tbeseda@gmail.com>
2023-09-06 13:07:01 -0600

    try waylon on blog index

--- app/elements/blog/list.mjs
+++ app/elements/blog/list.mjs
@@ -1,6 +1,9 @@
+import { Collection } from 'waylon'
+
 /** @type {import('@enhance/types').EnhanceElemFn} */
 export default function BlogList({ html, state: { store } }) {
 	const { articles = [] } = store
+	const collection = new Collection(articles)
 	return html`
 		<dl>
 			${articles
@@ -18,5 +21,16 @@ export default function BlogList({ html, state: { store } }) {
 				)
 				.join('')}
 		</dl>
+
+		<h3 style="margin-top: 15rem;">
+			<a href="https://www.npmjs.com/package/waylon" target="_blank">Waylon Collection</a>
+			tests
+		</h3>
+		<h4><code>list()</code>:</h4>
+		${collection.list(
+			({ title, slug }) => `<a href="/blog/${slug}">${title}</a>`,
+		)}
+		<h4><code>table()</code>:</h4>
+		${collection.table()}
 	`
 }
