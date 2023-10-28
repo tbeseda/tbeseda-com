44a262d1d3caba8fbe775afab83d1a7d9517be9f
Taylor Beseda <tbeseda@gmail.com>
2023-10-27 19:44:08 -0600

    add header-timers

--- app/api/blog/index.mjs
+++ app/api/blog/index.mjs
@@ -1,10 +1,14 @@
 import arc from '@architect/functions'
+import HeaderTimers from 'header-timers'
 import standardMiddleware from '../../middleware/common.mjs'
 
 const { articles } = await arc.tables()
 
 /** @type {import('@enhance/types').EnhanceApiFn} */
 async function getHandler ({ icon = '⛔️', hCards = [], currentlyPlaying }) {
+  const timers = HeaderTimers()
+
+  timers.start('dynamo', 'get articles')
   // TODO: not scan
   const query = await articles.scan({
     Limit: 100,
@@ -14,12 +18,18 @@ async function getHandler ({ icon = '⛔️', hCards = [], currentlyPlaying }) {
       '#date': 'date'
     }
   })
+  timers.stop('dynamo')
 
+  timers.start('sort', 'articles sort')
   const sortedArticles = query.Items.filter(({ published }) => published).sort(
     (a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf()
   )
+  timers.stop('sort')
 
   return {
+    headers: {
+      [timers.headerKey]: timers.headerValue()
+    },
     json: { icon, hCards, currentlyPlaying, articles: sortedArticles }
   }
 }
