46f7ddaca0e1f7a815793e9d9186e11f32ce1b51
Taylor Beseda <tbeseda@gmail.com>
2023-12-31 15:47:43 -0700

    sekret route layout

--- app/elements/sekret/login-or-menu.mjs
+++ app/elements/sekret/login-or-menu.mjs
@@ -3,12 +3,17 @@ export default function SekretLoginOrMenu ({ html, state: { store } }) {
   let { authorized } = store
   authorized = !!authorized
 
+  const menu = [
+    { href: '/sekret/blog', text: 'Blog Admin' },
+    { href: '/auth/spotify/login', text: 'Spotify Auth' },
+  ]
+
   return html`
     <h1>Sekret</h1>
 
     ${
       authorized
-        ? '<a href="/sekret/blog">Manage Blog</a>'
+        ? menu.map(({ href, text }) => `<a href="${href}">${text}</a>`).join('')
         : /* html */ `
           <form method="post">
             <label for="password">Password</label>
