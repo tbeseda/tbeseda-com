cc65a810e104e01c3c0c9b30a1d0b9e09067551d
Taylor Beseda <tbeseda@gmail.com>
2024-03-21 13:09:02 -0600

    start og-img endpoint

--- src/http/get-og_img-000slug/index.mjs
+++ src/http/get-og_img-000slug/index.mjs
@@ -1,49 +1,50 @@
-// learn more about HTTP functions here: https://arc.codes/http
-export async function handler(req) {
-  return {
-    statusCode: 200,
-    headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8',
+import fs from 'node:fs'
+import arc from '@architect/functions'
+import { Resvg } from '@resvg/resvg-js'
+import satori from 'satori'
+
+const fontLocation = new URL('./Roboto-Bold.ttf', import.meta.url)
+
+const svg = await satori(
+  {
+    type: 'div',
+    children: [],
+    key: null,
+    props: {
+      style: { color: 'black' },
+      children: 'hello, world',
     },
-    body: `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Architect</title>
-  <style>
-     * { margin: 0; padding: 0; box-sizing: border-box; } body { font-family: -apple-system, BlinkMacSystemFont, sans-serif; } .max-width-320 { max-width: 20rem; } .margin-left-8 { margin-left: 0.5rem; } .margin-bottom-16 { margin-bottom: 1rem; } .margin-bottom-8 { margin-bottom: 0.5rem; } .padding-32 { padding: 2rem; } .color-grey { color: #333; } .color-black-link:hover { color: black; }
-  </style>
-</head>
-<body class="padding-32">
-  <div class="max-width-320">
-    <img src="https://assets.arc.codes/logo.svg" />
-    <div class="margin-left-8">
-      <div class="margin-bottom-16">
-        <h1 class="margin-bottom-16">
-          Hello from an Architect Node.js function!
-        </h1>
-        <p class="margin-bottom-8">
-          Get started by editing this file at:
-        </p>
-        <code>
-          src/http/get-og_img-000slug/index.mjs
-        </code>
-      </div>
-      <div>
-        <p class="margin-bottom-8">
-          View documentation at:
-        </p>
-        <code>
-          <a class="color-grey color-black-link" href="https://arc.codes">https://arc.codes</a>
-        </code>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
-`,
+  },
+  {
+    fonts: [
+      {
+        name: 'Roboto',
+        data: fs.readFileSync(fontLocation.pathname),
+      },
+    ],
+    height: 100,
+    width: 300,
+  },
+)
+
+const resvg = new Resvg(svg)
+const pngData = await resvg.render()
+const pngBuffer = pngData.asPng()
+
+async function http({ pathParameters }) {
+  const { slug } = pathParameters
+  console.log('slug', slug)
+
+  return {
+    status: 200,
+    compression: false,
+    headers: {
+      'content-type': 'image/png',
+      'cache-control': 'public, max-age=86400',
+    },
+    body: pngBuffer,
   }
 }
+
+// @ts-ignore
+export const handler = arc.http.async(http)
