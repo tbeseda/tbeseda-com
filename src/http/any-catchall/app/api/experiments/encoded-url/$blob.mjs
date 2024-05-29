20d243830f9b32ba08c0378aa9607b584cc64b6b
Taylor Beseda <tbeseda@gmail.com>
2024-05-28 23:20:57 -0600

    encoded-url experiment

---
+++ src/http/any-catchall/app/api/experiments/encoded-url/$blob.mjs
@@ -0,0 +1,9 @@
+import { urlsafeDecode } from '@architect/shared/urlsafe-encode.mjs'
+
+/** @type {import('@enhance/types').EnhanceApiFn} */
+export async function get(req) {
+  const {
+    params: { blob },
+  } = req
+  return { json: { string: urlsafeDecode(blob) } }
+}
