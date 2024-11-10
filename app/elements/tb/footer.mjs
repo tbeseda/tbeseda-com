fc555477c62e92836338bb859655ea3ae536288f
Taylor Beseda <tbeseda@gmail.com>
2024-11-10 09:54:09 -0700

    bluesky link

--- app/elements/tb/footer.mjs
+++ app/elements/tb/footer.mjs
@@ -41,6 +41,71 @@ export default function TbFooter({ html, state: { store } }) {
         border-right: none;
         padding-right: 0;
       }
+
+      .bluesky-flutter {
+        display: inline-flex;
+        gap: 0.5em;
+        align-items: center;
+      }
+
+      .bluesky-flutter svg {
+        width: 1.25rem;
+        height: 1.25rem;
+        transition: 200ms;
+      }
+
+      .bluesky-flutter .left {
+        transform-origin: center;
+      }
+      .bluesky-flutter .right {
+        transform-origin: center;
+        transform: scale(-1, 1);
+      }
+      .bluesky-flutter:hover .left,
+      .bluesky-flutter:focus .left {
+        animation: flutter 430ms ease-in-out;
+        --flip: 1;
+      }
+      .bluesky-flutter:hover .right,
+      .bluesky-flutter:focus .right {
+        animation: flutter 500ms ease-in-out;
+        --flip: -1;
+      }
+      .bluesky-flutter:hover svg,
+      .bluesky-flutter:focus svg{
+        transform: rotate(-5deg);
+        transition: 500ms;
+      }
+
+      @media (prefers-reduced-motion) {
+        .bluesky-flutter:hover .left,
+        .bluesky-flutter:focus .left,
+        .bluesky-flutter:hover .right,
+        .bluesky-flutter:focus .right {
+          animation: none;
+        }
+      }
+
+      @keyframes flutter {
+        10% {
+          transform: scale(calc(var(--flip)*1), 0.9);
+        }
+        20% {
+          transform: scale(calc(var(--flip)*0.5), 1)
+        }
+        40% {
+          transform: scale(calc(var(--flip)*0.9), 0.95);
+        }
+        60% {
+          transform: scale(calc(var(--flip)*0.3), 1);
+        }
+        80% {
+          transform: scale(calc(var(--flip)*0.9), 0.95);
+        }
+        100% {
+          transform: scale(calc(var(--flip)*1), 1);
+        }
+      }
     </style>
 
     <footer>
@@ -50,10 +115,20 @@ export default function TbFooter({ html, state: { store } }) {
 
       ${currentlyPlaying?.item ? '<tb-spotify-playing></tb-spotify-playing>' : `<div>${icon}</div>`}
 
-      <div class="say-hi">
-        Say "hi" on
-        <a class="secondary" rel="me" href="https://indieweb.social/@tbeseda">Mastodon</a>
-      </div>
+      <a href="https://bsky.app/profile/tbeseda.com" target="_blank" class="bluesky-flutter">
+
+        <svg id="flutterby" class="bluesky-flutter" viewBox="0 0 566 500" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <path id="wing" fill="currentColor" d="M 123.244 35.008 C 188.248 83.809 283.836 176.879 283.836 235.857 C 283.836 316.899 283.879 235.845 283.836 376.038 C 283.889 375.995 282.67 376.544 280.212 383.758 C 266.806 423.111 214.487 576.685 94.841 453.913 C 31.843 389.269 61.013 324.625 175.682 305.108 C 110.08 316.274 36.332 297.827 16.093 225.504 C 10.271 204.699 0.343 76.56 0.343 59.246 C 0.343 -27.451 76.342 -0.206 123.244 35.008 Z" />
+          </defs>
+          <use xlink:href="#wing" class="left" />
+          <use xlink:href="#wing" class="right" />
+        </svg>
+
+        @tbeseda.com
+
+      </a>
+
     </footer>
 
     <server-timings sep=": " top="3" exclude="cdn-*,enhance-*"></server-timings>
